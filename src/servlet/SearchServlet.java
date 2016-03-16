61181a662c147641fe335e208d7cb2c2c31fc939
atillaahun <szikora.atilla@gmail.com>
2016-03-16 14:15:37 +0100

    given skills in servlet converted into a Set

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -5,11 +5,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SearchServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        req.getRequestDispatcher("index.html").include(req, resp);
+
+        String[] skills = req.getParameter("skills").split(",");
+        Set skillSet = new HashSet<>(Arrays.asList(skills));
+        out.println("Given skills: " + skillSet);
+
 
     }
 }
